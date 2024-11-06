55b48b926401b49a77be8babb1f466658a33b000
TabotAlistar-gift <alistargift195@gmail.com>
2024-11-06 07:38:11 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "HELLO LEVEL 400 FET,QUALITY ASSURANCE!"})
+
+if __name__ == '__main__':
+    app.run()
